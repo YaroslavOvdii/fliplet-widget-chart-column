203b860c183e74d7b61a2b1758797c4e5af10078
Hugo Carneiro <hcarneiro@weboo.biz>
2016-11-01 15:58:39 +0000

    WIP - Build Development

--- js/build.js
+++ js/build.js
@@ -1,63 +1,117 @@
-$('[data-poll-results-id] .poll-container').each(function () {
-  var $el = $(this);
+$('[data-poll-results-id]').each(function () {
+  var data = Fliplet.Widget.getData( $(this).data('poll-results-id') );
+  var organizationId = Fliplet.Env.get('organizationId');
+  var ignoreDataSourceTypes = ['menu']; // Ignores Menus on the data sources
 
-  $el.highcharts({
-    chart: {
-      type: 'column'
-    },
-    title: {
-      text: 'Question label'
-    },
-    subtitle: {
-      text: 'A subtitle can go here'
-    },
-    xAxis: {
-      categories: [
-        'Yes',
-        'No',
-        'Maybe'
-      ],
-      crosshair: true,
-      gridLineWidth: 0
-    },
-    yAxis: {
-      min: 0,
-      title: {
-        text: 'Number of replies'
+  var $el = $(this).find('.poll-container');
+  data.entries = [];
+
+  // TODO: REFRESH SHOULD START here
+  // GETS DATA SOURCES
+  Fliplet.DataSources.get({
+    organizationId: organizationId
+  }).then(function (dataSources) {
+    // FILTER DATA SOURCES TO EXCLUDE MENUS
+    var filteredDataSources = dataSources.filter(function (dataSource) {
+      for (var i = 0; i < ignoreDataSourceTypes.length; i++) {
+        if (ignoreDataSourceTypes[i] === dataSource.type) {
+          return false;
+        }
+      }
+      return true;
+    });
+
+    // GETS THE ROWS OF THE DATA SOURCE
+    Promise.all(filteredDataSources.map(function (dataSource) {
+      return Fliplet.DataSources.connect(dataSource.id).then(function (source) {
+        return source.find();
+      }).then(function (rows) {
+        dataSource.rows = rows.map(function (row) {
+          return row.data;
+        });
+        return Promise.resolve(dataSource);
+      });
+    })).then(function (dataSources) {
+      // GETS ALL THE ROWS FOR A SPECIFIC COLUMN
+      dataSources.forEach(function (dataSource) {
+        if ( dataSource.hasOwnProperty('id') && dataSource.id == data.dataSourceId ) {
+          dataSource.rows.forEach(function(row) {
+            data.entries.push(row[data.dataSourceColumn]);
+          });
+        }
+      });
+      // SAVES THE TOTAL NUMBER OF ROW/ENTRIES
+      data.totalEntries = data.entries.length;
+
+      // TODO:
+      // - SEARCH data.entries FOR DUPLICATES
+      // - CREATE ARRAY/OBJECT THAT AGGRAGATES UNIQUE ENTRIES WITH THE NUMBER OF ROWS THAT ENTRY HAS
+
+      drawChart();
+    });
+  });
+
+  function drawChart() {
+    $el.highcharts({
+      chart: {
+        type: 'column'
       },
-      labels: {
+        title: {
+        text: '',
         enabled: false
       },
-      gridLineWidth: 0
-    },
-    tooltip: {
-      enabled: false,
-      headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-      pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        '<td style="padding:0"><b>{point.y}</b></td></tr>',
-      footerFormat: '</table>',
-      shared: true,
-      useHTML: true
-    },
-    plotOptions: {
-      column: {
-        pointPadding: 0.2,
-        borderWidth: 0
+      subtitle: {
+        text: '',
+        enabled: false
+      },
+      xAxis: {
+        categories: data.entries,
+        labels: {
+          enabled: data.show_data_legend
+        },
+        crosshair: true,
+        gridLineWidth: 0
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: data.y_axix_title,
+          enabled: (data.y_axix_title !== '' ? true : false)
+        },
+        labels: {
+          enabled: false
+        },
+        gridLineWidth: 0
+      },
+      tooltip: {
+        enabled: false,
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+          '<td style="padding:0"><b>{point.y}</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: [{
+        name: data.x_axix_title,
+        data: [25, 15, 60, 25, 15, 60], //THIS NEEDS TO BE UPDATED TO SHOW THE TOTAL NUMBER OF EACH UNIQUE ENTRY
+        color: '#3276b1',
+        dataLabels: {
+          enabled: data.show_data_values,
+          color: '#333333',
+          align: 'center',
+          format: '{point.y}'
+        }
+      }],
+      legend: {
+        enabled: (data.x_axix_title !== '' ? true : false)
       }
-    },
-    series: [{
-      name: 'Answers',
-      data: [25, 15, 60],
-      color: '#3276b1',
-      dataLabels: {
-        enabled: true,
-        color: '#333333',
-        align: 'center',
-        format: '{point.y}'
-      }
-    }],
-    legend: {
-      enabled: false
-    }
-  });
+    });
+  }
 });
--- js/interface.js
+++ js/interface.js
@@ -10,6 +10,7 @@ var data = Fliplet.Widget.getData() || {
 var $dataSource = $('select#select-data-source');
 var $dataColumns= $('select#select-data-column');
 var organizationId = Fliplet.Env.get('organizationId');
+var ignoreDataSourceTypes = ['menu'];
 
 // Fired from Fliplet Studio when the external save button is clicked
 Fliplet.Widget.onSaveRequest(function () {
@@ -21,29 +22,52 @@ Fliplet.Widget.onSaveRequest(function () {
     y_axix_title: $('#yaxix_title').val(),
     x_axix_title: $('#xaxix_title').val(),
     show_total_entries: ($('#show_total_entries:checked').val() === "show" ? true : false),
-    auto_refresh: ($('#auto_refresh:checked').val() === "refresh" ? true : false)
+    auto_refresh: ($('#auto_refresh:checked').val() === "refresh" ? true : false),
+    dataSources: data.dataSources
   }).then(function () {
     Fliplet.Widget.complete();
   });
 });
 
+// GET DATA SOURCES, EXCLUDE MENUS, GET ALL THE ROWS
 Fliplet.DataSources.get({
   organizationId: organizationId
 }).then(function (dataSources) {
-  data.dataSources = dataSources;
-  dataSources.forEach(function (dataSource) {
-    $dataSource.append('<option value="' + dataSource.id + '">' + dataSource.name + '</option>');
+  var filteredDataSources = dataSources.filter(function (dataSource) {
+    for (var i = 0; i < ignoreDataSourceTypes.length; i++) {
+      if (ignoreDataSourceTypes[i] === dataSource.type) {
+        return false;
+      }
+    }
+    return true;
   });
 
-  // LOADS DATA SOURCE DATA
-  // NEEDS TO BE DONE HERE BECAUSE THE SELECT BOX NEEDS TO BE DYNAMIC UPDATED
-  if (data.dataSourceId) {
-    $dataSource.val(data.dataSourceId).trigger('change');
-    showColumnSelect();
-  }
-  if ( data.dataSourceId && data.dataSourceColumn ) {
-    $dataColumns.val(data.dataSourceColumn).trigger('change');
-  }
+  Promise.all(filteredDataSources.map(function (dataSource) {
+    return Fliplet.DataSources.connect(dataSource.id).then(function (source) {
+      return source.find();
+    }).then(function (rows) {
+      dataSource.rows = rows.map(function (row) {
+        return row.data;
+      });
+      return Promise.resolve(dataSource);
+    });
+  })).then(function (dataSources) {
+    data.dataSources = dataSources;
+    dataSources.forEach(function (dataSource) {
+      // APPEND DATA SOURCES NAMES AND ID
+      $dataSource.append('<option value="' + dataSource.id + '">' + dataSource.name + '</option>');
+    });
+
+    // LOADS DATA SOURCE DATA
+    // NEEDS TO BE DONE HERE BECAUSE THE SELECT BOX NEEDS TO BE DYNAMIC UPDATED
+    if (data.dataSourceId) {
+      $dataSource.val(data.dataSourceId).trigger('change');
+      showColumnSelect();
+    }
+    if ( data.dataSourceId && data.dataSourceColumn ) {
+      $dataColumns.val(data.dataSourceColumn).trigger('change');
+    }
+  });
 });
 
 // LOAD CHART SETTINGS
