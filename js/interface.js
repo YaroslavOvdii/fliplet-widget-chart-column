11416d25cb76c19ac20bf038768bc0d1664cd2bb
Andrey Riazantsev <71251809+AndrRyaz@users.noreply.github.com>
2020-11-04 13:09:47 +0200

    add access rules for chart

--- js/interface.js
+++ js/interface.js
@@ -57,6 +57,15 @@ var dsQueryData = {
       }
     ]
   },
+  accessRules: [
+    {
+      allow: 'all',
+      enabled: true,
+      type: [
+        'select'
+      ]
+    }
+  ],
   result: data.dataSourceQuery
 };
 
